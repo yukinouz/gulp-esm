cda2452530aa2ca22bd82759600ca097b3acb9a4
yukinouz1 <karl.vanc0uver1st@gmail.com>
2024-04-05 13:37:27 +0900

    [add]postcssプラグイン

--- tasks/compile-sass.mjs
+++ tasks/compile-sass.mjs
@@ -1,13 +1,17 @@
 import { src, dest } from "gulp";
 import * as dartSass from "sass";
-import gulpSass from "gulp-sass";
-import paths from "./paths.mjs";
-
 const sass = gulpSass(dartSass);
+import gulpSass from "gulp-sass";
+import gulpPostCss from "gulp-postcss";
+import autoprefixer from "autoprefixer";
+import cssnanoPlugin from "cssnano";
+import paths from "./paths.mjs";
+const postcssPlugins = [autoprefixer(), cssnanoPlugin()];
 
 const compileSass = () => {
   return src(paths.src.scss, { sourcemaps: true })
     .pipe(sass({ includePaths: ["src/scss", "node_modules"] }))
+    .pipe(gulpPostCss(postcssPlugins))
     .pipe(dest(paths.dist.scss, { sourcemaps: "sourcemaps" }));
 };
 
